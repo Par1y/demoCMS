2054cf33c7a058825c53ae46d49baceacff2efad
Par1y <xzfiszoombie@163.com>
2024-09-12 16:19:14 +0800

    fix bugs

--- src/main/resources/static/login_script.js
+++ src/main/resources/static/login_script.js
@@ -1,44 +1,35 @@
 document.addEventListener( "DOMContentLoaded", function() {
-    // 当用户输入email时，取消原始的默认值
-    document.getElementById('username').addEventListener( "focus", function() {
-        if (this.value === "username") {
-            this.value = "";
-            var pwd = document.getElementById("password");
-            if (pwd.value === "password") {
-                pwd.value = "";
-            }
-        }
-    })
-
-    // 当用户输入password时，取消原始的默认值
-    document.getElementById("password").addEventListener( "focus", function() {
-        if  (this.value === "password") {
-            this.value = "";
-        }
-    })
-
     // 跳转注册页面
     document.getElementById("register").addEventListener( "click", function() {
         window.location.href = "./register.html";
     })
 } );
 
-// 登录页面：get方法查询id
+// 登录页面：post
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // 阻止默认的表单提交行为
+    event.preventDefault(); 
+
      // 获取用户输入的数据
      const username = document.getElementById('username').value;
      const password = document.getElementById('password').value;
  
-     // 使用 axios 发送 GET 请求
-     axios.get('/user/login', {
-         params: {
-             username: username,
-             password: password
-         }
+     // 使用 axios 发送 POST 请求
+    const params = new URLSearchParams();
+    params.append("username", username);
+    params.append("password", password);
+
+     axios.post('/user/login', params, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
      })
-     .then(function(response) {
-         console.log(response.data); 
+     .then(res => {
+         if (res.data.code === 0) {
+            alert(`登录成功，欢迎您 ${username}!`);
+         } 
+         else {
+            alert(`登录失败: ${res.data.message}`);
+        }
      })
      .catch(function(error) {
          console.error('Error fetching user ID:', error);
--- src/main/resources/static/register_script.js
+++ src/main/resources/static/register_script.js
@@ -1,33 +1,4 @@
 document.addEventListener( "DOMContentLoaded", function() {
-    // 当用户输入email时，取消原始的默认值
-    document.getElementById('username').addEventListener( "focus", function() {
-        if (this.value === "username") {
-            this.value = "";
-            var pwd = document.getElementById("password");
-            var confirm_pwd = document.getElementById("confirm-password");
-            if (pwd.value === "password") {
-                pwd.value = "";
-            }
-            if (confirm_pwd.value === "password") {
-                confirm_pwd.value = "";
-            }
-        }
-    })
-
-    // 当用户输入password时，取消原始的默认值
-    document.getElementById("password").addEventListener( "focus", function() {
-        if  (this.value === "password") {
-            this.value = "";
-        }
-    })
-
-    // 当用户输入password时，取消原始的默认值
-    document.getElementById("confirm-password").addEventListener( "focus", function() {
-        if  (this.value === "password") {
-            this.value = "";
-        }
-    })
-
     // 跳转登录页面
     document.getElementById("login").addEventListener( "click", function() {
         window.location.href = "./login.html";
@@ -35,7 +6,6 @@ document.addEventListener( "DOMContentLoaded", function() {
 } );
 
 // 注册页面，传用户名和密码
-const params = new URLSearchParams();
 document.getElementById("register-submit").addEventListener( "submit", function(event) {
     event.preventDefault();
 
@@ -45,21 +15,29 @@ document.getElementById("register-submit").addEventListener( "submit", function(
 
     if(password !== confirmPassword) {
         alert("Passwords do not match");
+        document.getElementById("confirm-password").value = '';
+        document.getElementById("password").value = "";
         return;
     }
 
+    const params = new URLSearchParams();
     params.append('username', username);
     params.append('password', password);
 
     axios.post("/user/register", params, {
         headers: {
-            'Content-Type': 'application/x-www-form-unlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
     })
     .then(res => {
-        console.log(res.data);
+        if (res.code === 0) {
+            window.location.href="./login.html";
+        }
+        else {
+            alert("Registration failed: " + res.data.message);
+        }
     })
     .catch(err => {
-        console.log(err);
-    })
+        console.error("Error during registration:", err);
+    });
 })
